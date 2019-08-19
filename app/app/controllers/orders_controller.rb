11868499356e75721b77f2cb7e81ec9c721c9fc7
irockmysock <benny_chin89@hotmail.com>
2019-08-20 00:01:39 +0800

    completed delete item in order function

--- app/app/controllers/orders_controller.rb
+++ app/app/controllers/orders_controller.rb
@@ -4,4 +4,42 @@ class OrdersController < ApplicationController
     p "--------------------"
     p @orders
   end
+
+  def show
+    @order = Order.find(params[:id])
+  end
+
+  def update
+
+    @order = Order.find(params[:id])
+
+    @order.update(order_params)
+    p "UPDATEDDDDD"
+
+    redirect_to orders_path
+  end
+
+
+  def destroy
+
+    @order = Order.find(params[:id])
+
+    item = @order.items.find(params[:id])
+
+    if item
+        @order.items.delete(item)
+    end
+
+    redirect_to orders_path
+  end
+
+
+private
+
+  def order_params
+    params.require(:order).permit(:completed, :table_id)
+  end
+
+
+
 end
--- app/config/routes.rb
+++ app/config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
 
   get '/orders' => 'orders#index', as: 'orders'
+  get '/orders/:id' => 'orders#show' , as: 'order'
+  patch '/orders/:id' => 'orders#update'
+  delete '/orders/:id' => 'orders#destroy'
 
 end
