4e708cac758ab8c3e98d4f2734eaf9ec94385336
marcus <marcusteo@hotmail.com>
2019-08-20 11:18:05 +0800

    added kitchen view added javascript to kitchen view to pull data from database added completed button to mark orders as completed

--- app/app/controllers/mains_controller.rb
+++ app/app/controllers/mains_controller.rb
@@ -3,4 +3,25 @@ class MainsController < ApplicationController
     puts '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
     puts current_staff.inspect
   end
+  def kitchen
+    @orders = Order.where(completed: nil)
+  end
+  def orders
+    puts "working here~"
+    @orders = Order.all
+
+    puts @orders.inspect
+
+    # where("completed != true")
+    render json: @orders
+  end
+  def complete
+    p '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
+    puts request.query_string
+    p '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
+    order1 = Order.find(request.query_string)
+    order1.completed = true
+    order1.save
+    p '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
+  end
 end
--- app/config/routes.rb
+++ app/config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   get '/orders' => 'orders#index', as: 'orders'
 
   root "mains#index"
+  get '/kitchen' => 'mains#kitchen'
+  get '/getorders' => 'mains#orders'
+  get '/completeorders' => 'mains#complete'
   devise_for :staffs
   resources :tables
 
