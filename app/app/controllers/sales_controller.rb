43fe12094dea1de431a3ceb2b452100b8d9ad14c
irockmysock <benny_chin89@hotmail.com>
2019-08-20 21:34:39 +0800

    updated show sales view to display quantity

---
+++ app/app/controllers/sales_controller.rb
@@ -0,0 +1,15 @@
+class SalesController < ApplicationController
+  def show
+    @orders = Order.all.where(sales_id: params[:id])
+    @sale = Sale.find(params[:id])
+    # @qty = ItemsOrder.all.where({order_id: @orders[0][:id], item_order: item})
+    @qty = ItemsOrder.all.joins(:order, :item).where('orders.sales_id' => params[:id])
+    p "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+    p @qty
+    p "----------------------------------"
+    p @orders
+    p "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+  end
+
+
+end
