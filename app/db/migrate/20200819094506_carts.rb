bcbe7280bb5bf200e2deeb6e1f6063581175af05
DonC24 <51435215+DonC24@users.noreply.github.com>
2019-08-20 15:57:55 +0800

    Added carts table

---
+++ app/db/migrate/20200819094506_carts.rb
@@ -0,0 +1,10 @@
+class Carts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :carts do |t|
+      t.references :table
+      t.references :items
+      t.integer :item_quantity
+      t.timestamps
+    end
+  end
+end
