7f76e8f6ae2853f7d62e3709b7cc7e32ca913968
irockmysock <benny_chin89@hotmail.com>
2019-08-20 21:37:20 +0800

    updated show sales route changes

--- app/config/routes.rb
+++ app/config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
 
 
+  get '/items/optionajax' => 'items#optionajax'
+  resources :items
+
   get '/orders' => 'orders#index', as: 'orders'
   get '/orders/:id' => 'orders#show' , as: 'order'
   patch '/orders/:id' => 'orders#update'
@@ -11,12 +14,16 @@ Rails.application.routes.draw do
 
   root "mains#index"
   get '/kitchen' => 'mains#kitchen'
+  get '/table' => 'mains#table'
   get '/getorders' => 'mains#orders'
+  get '/assigntable' => 'mains#assign', as: "assign"
+  get '/releasetable' => 'mains#release', as: "release"
   get '/completeorders' => 'mains#complete'
   devise_for :staffs
   resources :tables
 
   get '/reports' => 'reports#index', as: 'reports'
+  get '/sales/:id' => 'sales#show' , as: 'sale'
 
 
 end
