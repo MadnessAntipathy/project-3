346f84c05727a303f4fef4899be76553984e7d09
marcus <marcusteo@hotmail.com>
2019-08-23 15:40:01 +0800

    fixed bug whereby customers click back and can see previous page details

--- app/app/controllers/application_controller.rb
+++ app/app/controllers/application_controller.rb
@@ -1,7 +1,14 @@
 class ApplicationController < ActionController::Base
+  before_action :set_no_cache
   private
+  def set_no_cache
+    response.headers["Cache-Control"] = "no-cache, no-store, max-age=0, must-revalidate"
+    response.headers["Pragma"] = "no-cache"
+    response.headers["Expires"] = "Fri, 01 Jan 1990 00:00:00 GMT"
+  end
+
 # Overwriting the sign_out redirect path method
   def after_sign_out_path_for(resource_or_scope)
-    items_path
+    root_path
   end
 end
