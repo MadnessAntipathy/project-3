35a468c9a278450b8aa6ca57f78e532098dd990f
DonC24 <51435215+DonC24@users.noreply.github.com>
2019-08-24 15:04:17 +0800

    fixed spacing for cards in items. Can make patch request for qty in cartys, but no data.

--- app/app/controllers/carts_controller.rb
+++ app/app/controllers/carts_controller.rb
@@ -10,11 +10,32 @@ class CartsController < ApplicationController
     @purchases = Purchase.all
   end
 
+  def update
+    puts "add to cart"
+    p params
+    p request.query_string
+      @cart = Cart.find(params[:id])
+
+      @cart.update(:item_quantity => params[:quantity])
+    puts "finish update"
+
+    respond_to do |format|
+        format.html { render inline: "location.reload();" }
+    end
+  end
+
   def destroy
     @cart = Cart.find(params[:id])
+
     @cart.destroy
 
     redirect_to carts_path
+  end
+
+  private
+
+  def cart_params
+    params.require(:cart).permit(:table_id, :item_id, :item_quantity)
   end
 
 end
