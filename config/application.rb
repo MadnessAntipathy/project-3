90ee653b2f396edb3597679987f578e2a6c7284b
marcus <marcusteo@hotmail.com>
2019-08-25 13:24:12 +0800

    heroku commit

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,7 @@ module App
 
     config.time_zone = 'Singapore'
     config.active_record.default_timezone = :local
-
+    config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
